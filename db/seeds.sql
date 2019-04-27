085084ddcb4274810b5b10ee42c3ad940031bdce
Joe Staugaard <joestaugaard@gmail.com>
2019-04-27 12:00:05 -0400

    test upload

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('Plain', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('With Cheese', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Bacon', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Bacon Cheese', false);
+
+
---
+++ public/assets/js/burgers.js
@@ -0,0 +1,64 @@
+// Make sure we wait to attach our handlers until the DOM is fully loaded.
+$(function() {
+  $(".change-devoured").on("click", function(event) {
+    var id = $(this).data("id");
+    var newDevour = $(this).data("newdevour");
+    console.log("Devour button clicked");
+    var newDevourState = {
+      devoured: 1
+    };
+
+    // Send the PUT request.
+    $.ajax("/api/burgers/" + id, {
+      type: "PUT",
+       data: newDevourState
+ 
+    }).then(
+      function() {
+        console.log("changed devour to", newDevour);
+        // Reload the page to get the updated list
+        location.reload();
+      }
+    );
+  });
+
+  $(".create-form").on("submit", function(event) {
+    // Make sure to preventDefault on a submit event.
+    event.preventDefault();
+
+    var newBurger = {
+      burger_name: $("#ca").val().trim(),
+      devoured: 0
+    };
+
+    console.log("new burger name:"+newBurger.burger_name);
+    console.log("new burg dev : "+newBurger.devour);
+
+    // Send the POST request.
+    $.ajax("/api/burgers", {
+      type: "POST",
+      data: newBurger
+    }).then(
+      function() {
+        console.log("created new burger");
+        // Reload the page to get the updated list
+        location.reload();
+      }
+    );
+  });
+
+  // $(".delete-burger").on("click", function(event) {
+  //   var id = $(this).data("id");
+
+  //   // Send the DELETE request.
+  //   $.ajax("/api/burgers/" + id, {
+  //     type: "DELETE"
+  //   }).then(
+  //     function() {
+  //       console.log("deleted burger", id);
+  //       // Reload the page to get the updated list
+  //       location.reload();
+  //     }
+  //   );
+  // });
+});
